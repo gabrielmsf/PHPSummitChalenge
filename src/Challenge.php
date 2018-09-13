3212f380560fe7cedf076df5f3d09406ad14b073
Anderson Oliveira <anderson.oliveira@locaweb.com.br>
2018-09-12 22:43:58 -0300

    Create resolution for calculate percentage of used storage.

--- src/Challenge.php
+++ src/Challenge.php
@@ -2,10 +2,38 @@
 
 namespace Challenge;
 
+use \Challenge\Models\Folders;
+
 class Challenge
 {
-    public function calcFolderUsedPercent()
+    private $userId;
+
+    public function __construct($userId)
     {
-        return 0;
+        $this->userId = $userId;
+    }
+
+    public function calcFolderUsedPercent(Folders $folders)
+    {
+        $conversion = 1024;
+        $storageGB = intval(str_replace('GB', '', $folders->getStorageSpace($this->userId)));
+        $userFolders = $folders->list($this->userId);
+        $spaceUsed = $this->spaceUsed($userFolders);
+
+        $storageMB = $storageGB * $conversion;
+        $percentUsed = ($spaceUsed * 100) / $storageMB;
+
+        return ceil($percentUsed) . '%';
+    }
+
+    private function spaceUsed(array $userFolders)
+    {
+        $size = 0;
+
+        foreach ($userFolders as $folder) {
+            $size += intval(str_replace('MB', '', $folder->size));
+        }
+
+        return $size;
     }
 }
