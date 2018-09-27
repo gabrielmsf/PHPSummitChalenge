349260a78004036fbcc3dd18cfcbdb23db0b5486
Anderson Oliveira <anderson.oliveira@locaweb.com.br>
2018-09-27 19:58:11 -0300

    Teste de pareamento com o João Carlos.

--- src/Challenge.php
+++ src/Challenge.php
@@ -4,8 +4,38 @@ namespace Challenge;
 
 class Challenge
 {
-    public function calcFolderUsedPercent()
+    private $userId;
+
+    public function __construct($userId)
     {
-        return 0;
+        $this->userId = $userId;
+    }
+
+    public function calcFolderUsedPercent($folders)
+    {
+        $userFolders = $folders->list($this->userId);
+
+        $userUsage = $this->calcUserFolderUsage($userFolders);
+        $userStorage = $this->convertUserStorageSpace($folders->getStorageSpace($this->userId));
+
+        $percent = ceil($userUsage / $userStorage * 100);
+
+        return  $percent . '%';
+    }
+
+    protected function calcUserFolderUsage($userFolders)
+    {
+        $usage = 0;
+
+        foreach ($userFolders as $folder) {
+            $usage += (int) str_replace('MB', '', $folder->size);
+        }
+
+        return $usage;
+    }
+
+    protected function convertUserStorageSpace($userStorage)
+    {
+        return (int) str_replace('GB', '', $userStorage) * 1024;
     }
 }
