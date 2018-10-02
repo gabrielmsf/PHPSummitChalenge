5d29fd4fb6dde2c1e29d5be0f759ed97fa8dac36
Anderson Oliveira <anderson.oliveira@locaweb.com.br>
2018-10-02 14:23:35 -0300

    Pareamento com Alex Coelho.

--- src/Challenge.php
+++ src/Challenge.php
@@ -4,8 +4,29 @@ namespace Challenge;
 
 class Challenge
 {
-    public function calcFolderUsedPercent()
+    private $userId;
+
+    public function __construct($userId)
     {
-        return 0;
+        $this->userId = $userId;
+    }
+
+    public function calcFolderUsedPercent($folders)
+    {
+        $user_folders = $folders->list($this->userId);
+        $user_space = $folders->getStorageSpace($this->userId);
+        $user_space = intval(str_replace('GB', '', $user_space));
+        $total_size = 0;
+
+        foreach ($user_folders as $folder) {
+            $size_str = $folder->size;
+            $total_size += intval(str_replace('MB', '', $size_str));
+        }
+
+        $total_size = $total_size/1024;
+
+        $percentage = ceil(($total_size/$user_space) * 100);
+
+        return $percentage . '%';
     }
 }
